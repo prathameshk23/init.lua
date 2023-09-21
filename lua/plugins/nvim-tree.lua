291dab8e2231a7f4d71e38adab48342800e1cdaa
Prathamesh Kulkarni <prathameshkulkarni952@gmail.com>
2023-09-21 20:14:45 +0530

    Updated nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,15 +4,8 @@ return {
 	config = function()
 		local nvimtree = require("nvim-tree")
 
-		-- recommended settings from nvim-tree documentation
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
-
-		-- change color for arrows in tree to light blue
-		vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
-
-		-- configure nvim-tree
 		nvimtree.setup({
+			disable_netrw = false,
 			diagnostics = {
 				enable = true,
 				icons = {
@@ -22,54 +15,27 @@ return {
 					error = "",
 				},
 			},
-
-			view = {
-				width = 35,
-				relativenumber = true,
-			},
-			-- change folder arrow icons
-			renderer = {
-				indent_markers = {
-					enable = true,
-				},
-				icons = {
-					glyphs = {
-						folder = {
-							arrow_closed = "", -- arrow when folder is closed
-							arrow_open = "", -- arrow when folder is open
-						},
-					},
-				},
-			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-			filters = {
-				custom = { ".DS_Store" },
-			},
-			git = {
-				ignore = false,
-			},
 		})
+		local function open_nvim_tree(data)
+			-- buffer is a [No Name]
+			local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
 
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+			-- buffer is a directory
+			local directory = vim.fn.isdirectory(data.file) == 1
 
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-		keymap.set(
-			"n",
-			"<leader>ef",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			{ desc = "Toggle file explorer on current file" }
-		) -- toggle file explorer on current file
-		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
+			if not no_name and not directory then
+				return
+			end
+
+			-- change to the directory
+			if directory then
+				vim.cmd.cd(data.file)
+			end
+
+			-- open the tree
+			require("nvim-tree.api").tree.open()
+		end
+		vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+		vim.keymap.set("n", "<leader>pv", "<cmd>NvimTreeFindFileToggle<CR>")
 	end,
 }
